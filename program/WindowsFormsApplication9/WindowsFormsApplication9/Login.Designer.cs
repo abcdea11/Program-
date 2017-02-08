13d2bc5a45b452b634b6a619c539147cb6ee9c89
abcdea11 <bduda1@edu.cdv.pl>
2017-02-08 22:45:36 +0100

    Logowanie prace w trakcie 30%
    Zacząłem pracować nad logowaniem + bazą danych

--- program/WindowsFormsApplication9/WindowsFormsApplication9/Login.Designer.cs
+++ program/WindowsFormsApplication9/WindowsFormsApplication9/Login.Designer.cs
@@ -35,6 +35,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -46,6 +47,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -56,6 +58,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -81,7 +84,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(104, 31);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
@@ -89,23 +92,35 @@
             this.textBox2.Location = new System.Drawing.Point(104, 61);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '#';
-            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.Size = new System.Drawing.Size(191, 20);
             this.textBox2.TabIndex = 5;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(124, 87);
+            this.button3.Location = new System.Drawing.Point(201, 87);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 23);
+            this.button3.Size = new System.Drawing.Size(95, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = " Show password";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(104, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Hide password";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Logincs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(307, 192);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -114,7 +129,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Logincs";
-            this.Text = "Logincs";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Logincs_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -130,5 +145,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- program/WindowsFormsApplication9/WindowsFormsApplication9/Login.cs
+++ program/WindowsFormsApplication9/WindowsFormsApplication9/Login.cs
@@ -7,11 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApplication9
 {
     public partial class Logincs : Form
-    {
+    {    // metody // Łączenie z bazą
+
+        SqlConnection con = new SqlConnection();
+
+
+
+
+
+        //
+
+
+
+
+
+
         public Logincs()
         {
             InitializeComponent();
@@ -22,6 +37,31 @@ namespace WindowsFormsApplication9
 
 
             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox2.PasswordChar = char.MinValue;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox2.PasswordChar = '#';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Clear();
+            textBox1.Focus();                   // kursor automatycznie przechodzi do 1 text box
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.Hide();                    // ukrywanie obecnej formy
+            Form1 form1 = new Form1();
+            form1.Show();
         }
     }
 }
