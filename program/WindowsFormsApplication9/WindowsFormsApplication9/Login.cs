f3bb5a112d11b620450559ca24e1e8c7617ae82a
abcdea11 <bduda1@edu.cdv.pl>
2017-02-08 21:34:33 +0100

    Działa
    zobacz czy otworzysz

---
+++ program/WindowsFormsApplication9/WindowsFormsApplication9/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Logincs : Form
+    {
+        public Logincs()
+        {
+            InitializeComponent();
+        }
+
+        private void Logincs_Load(object sender, EventArgs e)
+        {
+
+
+            
+        }
+    }
+}
